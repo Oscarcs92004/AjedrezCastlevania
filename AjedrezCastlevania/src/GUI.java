a782d5bebf0218d9c447ccc5a22c0f7c9370fabe
Oscarcs92004 <oscarcs92004@gmail.com>
2026-08-05 15:41:31 -0600

    agrego parte del GUI a la nueva clase

--- AjedrezCastlevania/src/GUI.java
+++ AjedrezCastlevania/src/GUI.java
@@ -3,11 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 
 /**
  *
  * @author oscar
  */
-public class GUI {
+public class GUI extends JFrame{
     
+    private JButton iniciar;
+    private JButton crearJugador;
+    private JButton salir;
+    private JLabel titulo;
+    
+    private void inicializarComponentes(){
+        setTitle("Vampire Wargame");
+        setSize(1200,900);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        
+        titulo = new JLabel("Vampire Wargame", SwingConstants.CENTER);
+        titulo.setFont(new Font("Serif", Font.BOLD, 30));
+        panel.add(titulo, BorderLayout.NORTH);
+        
+        
+        setVisible(true);
+    }
+    
+    public GUI(){
+        inicializarComponentes();
+    }
 }
--- AjedrezCastlevania/src/Principal.java
+++ AjedrezCastlevania/src/Principal.java
@@ -12,5 +12,6 @@
 public class Principal{
     
     public static void main(String args[]){
+        GUI g = new GUI();
     }
 }
