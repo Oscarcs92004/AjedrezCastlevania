6f1ad4304e1399d2783d3438ee7b43a62a5d2bd5
Oscarcs92004 <oscarcs92004@gmail.com>
2026-08-10 18:48:04 -0600

    agrego fondo de pantalla al menu de inicio y cambio el tamano de los botones para que no cubran la pantalla entera

--- AjedrezCastlevania/src/GUI.java
+++ AjedrezCastlevania/src/GUI.java
@@ -4,9 +4,13 @@
  * and open the template in the editor.
  */
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
@@ -46,19 +50,30 @@ public class GUI extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         
-        panel = new JPanel();
-        panel.setLayout(new BorderLayout());
+         panel = new JPanel(new BorderLayout()) {
+            private Image imagenFondo = new ImageIcon(getClass().getResource("/Iconos/portadaJuego.png")).getImage();
+            @Override
+            protected void paintComponent(java.awt.Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(imagenFondo,0,0,getWidth(),getHeight(),this);
+            }
+        };
         
         titulo = new JLabel("Vampire Wargame", SwingConstants.CENTER);
         titulo.setFont(new Font("Serif", Font.BOLD, 30));
+        titulo.setForeground(Color.WHITE);
         panel.add(titulo, BorderLayout.NORTH);
         
         JPanel botones = new JPanel();
-        botones.setLayout(new GridLayout(3,1,20,20));
+        botones.setOpaque(false);
+        botones.setLayout(new GridLayout(3,1,0,15));
         iniciar = new JButton("Iniciar sesion");
         salir = new JButton("Salir");
         crearJugador = new JButton("Crear Jugador");
-        
+        Dimension tamanioBoton = new Dimension(250,60);
+        iniciar.setPreferredSize(tamanioBoton);
+        salir.setPreferredSize(tamanioBoton);
+        crearJugador.setPreferredSize(tamanioBoton);
         iniciar.setFont(new Font("Arial",Font.BOLD, 18));
         salir.setFont(new Font("Arial",Font.BOLD, 18));
         crearJugador.setFont(new Font("Arial",Font.BOLD, 18));
@@ -67,7 +82,12 @@ public class GUI extends JFrame{
         botones.add(crearJugador);
         botones.add(salir);
         
-        panel.add(botones);
+        JPanel contenedorBotones = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        
+        contenedorBotones.setOpaque(false);
+        contenedorBotones.add(botones);
+        
+        panel.add(contenedorBotones, BorderLayout.CENTER);
         add(panel);
         salir.addActionListener(e -> System.exit(0));
         
