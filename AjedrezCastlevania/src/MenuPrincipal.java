bc43ae5ec58c93a009b27c18d6b2a92fa93dad21
Oscarcs92004 <oscarcs92004@gmail.com>
2026-08-10 19:41:34 -0600

    agrego clase nuevapartida vacia y agrego mi menu principal sin logica en los botones solo en cerrar sesion

--- AjedrezCastlevania/src/MenuPrincipal.java
+++ AjedrezCastlevania/src/MenuPrincipal.java
@@ -14,6 +14,58 @@ import javax.swing.SwingConstants;
  *
  * @author oscar
  */
-public class MenuPrincipal {
+public class MenuPrincipal extends JPanel{
+    private GUI ventana;
+    private String usuario;
+    private JButton jugar;
+    private JButton miCuenta;
+    private JButton reportes;
+    private JButton cerrarSesion;
     
+    private void inicializar(){
+        setLayout(new BorderLayout());
+        JLabel titulo = new JLabel("Vampire Wargame",SwingConstants.CENTER);
+        titulo.setFont(new Font("Serif", Font.BOLD, 40));
+        add(titulo, BorderLayout.NORTH);
+        JLabel bienvenida = new JLabel("Bienvenido, " + usuario,SwingConstants.CENTER);
+        bienvenida.setFont(new Font("Arial", Font.BOLD, 22));
+        add(bienvenida, BorderLayout.CENTER);
+        JPanel panelBotones = new JPanel();
+        panelBotones.setLayout(new GridLayout(4,1,15,15));
+        jugar = new JButton("Jugar Vampire Wargame");
+        miCuenta = new JButton("Mi cuenta");
+        reportes = new JButton("Reportes");
+        cerrarSesion = new JButton("Cerrar sesión");
+        jugar.setFont(new Font("Arial", Font.BOLD, 18)); 
+        miCuenta.setFont(new Font("Arial", Font.BOLD, 18)); 
+        reportes.setFont(new Font("Arial", Font.BOLD, 18));
+        cerrarSesion.setFont(new Font("Arial", Font.BOLD, 18));
+        panelBotones.add(jugar);
+        panelBotones.add(miCuenta);
+        panelBotones.add(reportes);
+        panelBotones.add(cerrarSesion);
+        add(panelBotones, BorderLayout.SOUTH);
+        jugar.addActionListener(e->{
+            //ventana.cambiarPanel(); falta crear clase nueva partida
+        });
+        
+        miCuenta.addActionListener(e->{
+            // falta crear clase
+        });
+        
+        reportes.addActionListener(e->{
+            // falta crear clase
+        });
+        
+        cerrarSesion.addActionListener(e->{
+            ventana.mostrarMenu();
+        });
+    }
+    
+    public MenuPrincipal(GUI ventana, String usuario){
+        this.ventana = ventana;
+        this.usuario = usuario;
+        
+        inicializar();
+    }
 }
