72e9acde761436b3f22661531ae8453763291dcc
Oscarcs92004 <oscarcs92004@gmail.com>
2026-08-10 19:28:47 -0600

    agrego clase menu principal y le agrego los importas que voy a usar

---
+++ AjedrezCastlevania/src/MenuPrincipal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+/**
+ *
+ * @author oscar
+ */
+public class MenuPrincipal {
+    
+}
